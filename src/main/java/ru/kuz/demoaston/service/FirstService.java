ed6500a5bbfcf01eb691d37ce3f6082ced49864b
Mihail <mi.kuznecov@astondevs.ru>
2024-07-22 13:33:40 +0300

    develop update FirstService.java add str;

--- src/main/java/ru/kuz/demoaston/service/FirstService.java
+++ src/main/java/ru/kuz/demoaston/service/FirstService.java
@@ -1,9 +1,10 @@
-
+package ru.kuz.demoaston.service;
 
 public class FirstService {
- public String helloPage( String name,
+    public String helloPage(String name,
                             String surname
-                            ) {
-  String str;
-  return str;
+    ) {
+        String str = "";
+        return str;
+    }
 }
